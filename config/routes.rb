0cc5acedda119057359cb5d642a36f6fdae81252
Cyril Lin <bliteray@vip.qq.com>
2017-02-13 17:47:54 +0800

    add user modify

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     get '/', to: 'home#index'
   end
 
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
